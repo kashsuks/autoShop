0497ca5d2949cad667fd9815c199351101ea4a01
Kashyap Sukshavasi <ksukshavasi@gmail.com>
2024-10-24 19:43:16 -0400

    restarting again

--- backend/config/db.js
+++
@@ -1,7 +0,0 @@
-const { Pool } = require('pg');
-
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL, // This will pull the connection string from .env
-});
-
-module.exports = pool;
