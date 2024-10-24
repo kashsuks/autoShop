42b96922c8ee15959f8a36c25204e12956d5bf4d
Kashyap Sukshavasi <101945096+kashsuks@users.noreply.github.com>
2024-10-24 19:14:12 -0400

    Delete backend directory

--- backend/routes/shops.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const shopController = require('../controllers/shopController');
-const router = express.Router();
-
-router.get('/slots', shopController.getSlots);
-router.post('/register', shopController.registerShop);
-
-module.exports = router;
--- backend/services/geoService.js
+++
@@ -1,8 +0,0 @@
-const geolib = require('geolib');
-
-exports.calculateDistance = (lat1, lon1, lat2, lon2) => {
-  return geolib.getDistance(
-    { latitude: lat1, longitude: lon1 },
-    { latitude: lat2, longitude: lon2 }
-  );
-};
